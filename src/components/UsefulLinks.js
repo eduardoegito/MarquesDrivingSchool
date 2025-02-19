9d84cd17ea10df365f25659a1062f905914abbcf
Carlos Eduardo Egito <eduardoegito@gmail.com>
2025-02-18 18:53:53 -0800

    Update UsefulLinks.js

--- src/components/UsefulLinks.js
+++ src/components/UsefulLinks.js
@@ -9,7 +9,7 @@ const UsefulLinks = () => {
             <li>Prepare for your knowledge test. 
             </li>
           </a>
-          <a href=''>
+          <a href='#'>
             <li>You can also download the app.
             </li>
           </a>
